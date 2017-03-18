87d39e1e27421cd0da4a8038b11b97270e7b821b
Christopher Schnick <c.schnick@monospark.org>
2017-03-18 02:15:53 +0100

    Make Mark command toggleable

--- _mouse_control.py
+++ _mouse_control.py
@@ -24,8 +24,30 @@ def stop_marking():
     global marking
     if marking:
         marking = False
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
         Key("shift:up").execute()
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+
+
+def toggle_marking():
+    global marking
+    if marking:
+        stop_marking()
+    else:
+        start_marking()
+
+SCROLL_THRESHOLD = 3
+SCROLL_SPEED = 0.003
+
+
+def get_scroll_amount(moved):
+    if -SCROLL_THRESHOLD < moved < SCROLL_THRESHOLD:
+        return 0
+    if moved > 0:
+        value = moved - SCROLL_THRESHOLD
+        return int(SCROLL_SPEED * (value * value))
+    else:
+        value = moved + SCROLL_THRESHOLD
+        return int(-SCROLL_SPEED * (value * value))
 
 
 def call():
@@ -34,7 +56,7 @@ def call():
         x, y = win32api.GetCursorPos()
         dx = x - scroll_position[0]
         dy = y - scroll_position[1]
-        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, x, y, -dy / 2)
+        win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, x, y, get_scroll_amount(-dy))
 
 
 dragonfly.timer.timer.add_callback(call, 0.01)
@@ -50,9 +72,9 @@ def toggle_scroll():
 class MouseRule(MappingRule):
     mapping = {
         "scroll": Function(toggle_scroll),
-        "mark": Function(start_marking),
+        "mark": Function(toggle_marking),
         "(left-click|click)": Mouse("left"),
-        "(double-click|select word)": Mouse("left:down, left:up, left:down, left:up"),
+        "(double-click|select)": Mouse("left:down, left:up, left:down, left:up"),
         "(select line)": Mouse("left:down, left:up, left:down, left:up, left:down, left:up"),
         "(right-click)": Mouse("right")
     }
@@ -65,5 +87,5 @@ def unload():
     global grammar
     if grammar:
         grammar.unload()
+        dragonfly.timer.timer.remove_callback(call)
     grammar = None
-    dragonfly.timer.timer.remove_callback(call)
